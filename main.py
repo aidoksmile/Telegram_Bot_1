e49b95286db6f53ac6baa3535b9062110b668427
Aida <shakharova.aida@yandex.ru>
2022-12-03 12:55:04 +0600

    крестики нолики бот

---
+++ main.py
@@ -0,0 +1,50 @@
+import telebot
+import model
+
+API_TOKEN = "5951902081:AAE2LAVrSGhejcpHRDle5JXmT8yK4pD0WLs"
+bot = telebot.TeleBot(API_TOKEN)
+game_status = False
+player = 0
+sign = ['X', 'O']
+n = 3
+x_chat_id = ""
+board = list(range(1, 10))
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    global game_status
+
+    board = list(range(1, 10))
+    bot.send_message(message.chat.id, "Сыграем в крестики-нолики?\n")
+    bot.send_message(message.chat.id, model.Draw_board(board))
+    bot.send_message(message.chat.id, f"\nХодит {player+1}-й игрок\nНапишите номер ячейки?")
+    game_status = True
+    x_chat_id = message.chat.id
+
+@bot.message_handler(commands=['help'])
+def help_message(message):
+    message_text = "Для начала игры нажмите /start\n"
+    message_text += "Для вызова справки по командам нажмите /help"
+    bot.send_message(message.chat.id, message_text)
+
+@bot.message_handler()
+def game_message(message):
+    if game_status == False:
+        bot.send_message(message.chat.id, "Для начала игры нажмите /start\n")
+        return
+    player_symbol = int(message.text)
+    sign = "0"
+    if message.chat.id == x_chat_id:
+        sign = "X"
+    input_ans = model.Player_input(board, player_symbol, sign)
+    if input_ans != "":
+        bot.send_message(message.chat.id, input_ans)
+        return
+    if model.Check_win(board):
+        bot.send_message(message.chat.id, "Win!")
+        game_status = False
+        return 
+    model.Draw_board(board)
+
+print("Бот работает")
+bot.polling()
---
+++ model.py
@@ -0,0 +1,67 @@
+# Создайте программу для игры в ""Крестики-нолики""
+
+def Draw_board(board):
+    s = "-" * 13 + "\n"
+    print("-" * 13)
+    for i in range(3):
+        s = s + "|" + str(board[0 + i * 3]) + "|" + str(board[1 + i * 3]) + "|" + str(board[2 + i * 3]) + "|" + "\n"
+        s = s + "-" * 13
+        print("|", board[0 + i * 3], "|", board[1 + i * 3], "|", board[2 + i * 3], "|")
+        print("-" * 13)
+    return s
+
+def Player_input(board, player_symbol, sign):
+    if 1 <= player_symbol <= 9:
+        if str(board[player_symbol - 1]) not in "XO":
+                board[player_symbol - 1] = sign
+        else:
+                return "Эта клетка занята!"
+    else:
+            return "Вводить числа от 1 до 9."
+    # valid = False
+    # while not valid:
+    #     player_answer = input("Куда поставим " + player_symbol+"? ")
+    #     try:
+    #         player_answer = int(player_answer)
+    #     except:
+    #         print("Вводить числа от 1 до 9.")
+    #         continue
+    #     if 1 <= player_answer <= 9:
+    #         if str(board[player_answer - 1]) not in "XO":
+    #             board[player_answer - 1] = player_symbol
+    #             valid = True
+    #         else:
+    #             print("Эта клетка занята!")
+    #     else:
+    #         print("Вводить числа от 1 до 9.")
+
+def Check_win(board):
+    win_line = ((0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6), (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6))
+    for each in win_line:
+        if board[each[0]] == board[each[1]] == board[each[2]]:
+            return board[each[0]]
+    return False
+
+# def main(board):
+#     counter = 0
+#     win = False
+#     while not win:
+#         Draw_board(board)
+#         if counter % 2 == 0:
+#             Player_input("X")
+#         else:
+#             Player_input("O")
+#         counter += 1
+#         if counter > 4:
+#             tmp = Check_win(board)
+#             if tmp:
+#                 print(tmp, "победил!")
+#                 win = True
+#                 break
+#         if counter == 9:
+#             print("Ничья!")
+#             break
+#     Draw_board(board)
+
+# board = list(range(1, 10))
+# main(board)
